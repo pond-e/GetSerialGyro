fe3ad664cacbaca343a120655e09ce7600269655
pond-e <haruto2266@icloud.com>
2023-01-25 22:29:42 +0900

    first commit

---
+++ Assets/SerialCube.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SerialCube : MonoBehaviour
+{
+
+	public SerialHandler serialHandler;
+	public Text text;
+	public GameObject cube;
+
+	private float[] q_before = new float[4] { 0, 0, 0, 1 };
+	private float[] q = new float[4];
+
+	private float dt = 0.1f;
+
+	private float[] r_tmp = new float[3];
+
+	// Use this for initialization
+	void Start()
+	{
+		//信号を受信したときに、そのメッセージの処理を行う
+		serialHandler.OnDataReceived += OnDataReceived;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	/*
+	 * シリアルを受け取った時の処理
+	 */
+	void OnDataReceived(string message)
+	{
+		try
+		{
+			string[] angles = message.Split(',');
+			text.text = "x:" + angles[0] + "\n" + "y:" + angles[1] + "\n" + "z:" + angles[2] + "\n"; // シリアルの値をテキストに表示
+
+			// Vectorは前から順番にx,y,zだけど、そのままセットすると
+			// Unity上の回転の見た目が変になるので、y,zの値を入れ替えている。
+			//Vector3 angle = new Vector3(float.Parse(angles[0]), float.Parse(angles[2]), float.Parse(angles[1]));
+			//cube.transform.rotation = Quaternion.Euler(angle);
+
+			float x_value = -1.0f * float.Parse(angles[0]);
+			float y_value = float.Parse(angles[1]);
+			float z_value = float.Parse(angles[2]); // 右手系と左手系
+
+			/*
+			float[] w = { x_value, y_value, z_value };
+
+			float[] q_tmp = new float[4];
+			q_tmp = find_q(w_times_q(w, q_before), dt);
+			q[0] = q_before[0] + q_tmp[0];
+			q[1] = q_before[1] + q_tmp[1];
+			q[2] = q_before[2] + q_tmp[2];
+			q[3] = q_before[3] + q_tmp[3];
+
+			q_before = q;
+
+			Vector3 angle = new Vector3(q[0], q[1], q[2]);
+			this.transform.rotation = Quaternion.AngleAxis(q[3], angle);
+			*/
+
+			r_tmp = find_r(y_value, z_value, x_value);
+			Vector3 angle = new Vector3(cube.transform.localEulerAngles.x + r_tmp[0], cube.transform.localEulerAngles.y + r_tmp[1], cube.transform.localEulerAngles.z + r_tmp[2]);
+			this.transform.rotation = Quaternion.Euler(angle);
+
+
+		}
+		catch (System.Exception e)
+		{
+			Debug.LogWarning(e.Message);
+		}
+	}
+	static float[] w_times_q(float[] w, float[] q)
+	{
+		float[] product = new float[4];
+		product[0] = 0 * q[0] + w[2] * q[1] + -w[1] * q[2] + w[0] * q[3];
+		product[1] = -w[2] * q[0] + 0 * q[1] + w[0] * q[2] + w[1] * q[3];
+		product[2] = w[1] * q[0] + -w[0] * q[1] + 0 * q[2] + w[2] * q[3];
+		product[3] = -w[0] * q[0] + -w[1] * q[1] + -w[2] * q[2] + 0 * q[3];
+
+		return product;
+	}
+
+	static float[] find_q(float[] wq, float dt)
+	{
+		float[] result = new float[4];
+		result[0] = 0.5f * wq[0] * dt;
+		result[1] = 0.5f * wq[1] * dt;
+		result[2] = 0.5f * wq[2] * dt;
+		result[3] = 0.5f * wq[3] * dt;
+
+		return result;
+	}
+
+	float[] find_r(float x, float y, float z)
+    {
+		float[] result = new float[3];
+		float ganma = cube.transform.localEulerAngles.x;
+		float beta = cube.transform.localEulerAngles.y;
+
+		result[0] = 1 * (x * dt) + Mathf.Tan(beta * (Mathf.PI / 180))*Mathf.Cos(ganma * (Mathf.PI / 180))*(z*dt);
+		result[1] = Mathf.Cos(beta * (Mathf.PI / 180)) * (y * dt) - Mathf.Sin(ganma * (Mathf.PI / 180)) * (z * dt);
+		result[2] = (Mathf.Sin(ganma * (Mathf.PI / 180)) / Mathf.Cos(beta * (Mathf.PI / 180))) * (y * dt) + (Mathf.Cos(ganma * (Mathf.PI / 180)) / Mathf.Cos(beta * (Mathf.PI / 180))) * (z * dt);
+
+		return result;
+    }
+}
---
+++ Assets/SerialCube_q.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SerialCube_q : MonoBehaviour
+{
+
+	public SerialHandler serialHandler;
+	public Text text;
+	public GameObject cube;
+
+	private float[] q_before = new float[4] { 0, 0, 0, 1 };
+	private float[] q = new float[4];
+
+	private float dt = 0.1f;
+
+	private float[] r_tmp = new float[3];
+
+	// Use this for initialization
+	void Start()
+	{
+		//信号を受信したときに、そのメッセージの処理を行う
+		serialHandler.OnDataReceived += OnDataReceived;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	/*
+	 * シリアルを受け取った時の処理
+	 */
+	void OnDataReceived(string message)
+	{
+		try
+		{
+			string[] angles = message.Split(',');
+			text.text = "w:" + angles[0] + "\n" + "x:" + angles[1] + "\n" + "y:" + angles[2] + "\n" + "z:" + angles[3] + "\n"; // シリアルの値をテキストに表示
+
+			// Vectorは前から順番にx,y,zだけど、そのままセットすると
+			// Unity上の回転の見た目が変になるので、y,zの値を入れ替えている。
+			//Vector3 angle = new Vector3(float.Parse(angles[0]), float.Parse(angles[2]), float.Parse(angles[1]));
+			//cube.transform.rotation = Quaternion.Euler(angle);
+
+			/*
+			float x_value = -1.0f * float.Parse(angles[0]);
+			float y_value = float.Parse(angles[1]);
+			float z_value = float.Parse(angles[2]); // 右手系と左手系
+			*/
+			Vector3 angle = new Vector3(float.Parse(angles[1]), -1.0f*float.Parse(angles[3]), float.Parse(angles[2]));
+			cube.transform.rotation = Quaternion.AngleAxis(180f*float.Parse(angles[0]), angle);
+		}
+		catch (System.Exception e)
+		{
+			Debug.LogWarning(e.Message);
+		}
+	}
+}
